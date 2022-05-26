bc7ac96ac353f2604230448df869d61d9f9fefd7
JCanizalesV <venturadaniel71@gmail.com>
2022-05-25 23:20:11 -0600

    Agregar archivos de proyecto.

---
+++ main/main.cpp
@@ -0,0 +1,22 @@
+#include <mysql.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    MYSQL* conectar;
+    conectar = mysql_init(0);
+    conectar = mysql_real_connect(conectar, "localhost", "root", "123456", "proyecto final super mercado", 3306, NULL, 0);
+
+    if (conectar) {
+        cout << "LA CONEXION SE REALIZO CON EXITO" << endl;
+    }
+    else {
+        cout << "NO SE PUDO REALIZAR LA CONEXION" << endl;
+    }
+
+    system("pause");
+    return 0;
+}
