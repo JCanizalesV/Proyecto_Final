08a5092acf7da869e3c641cd7868b5d137b01506
Jonatan-Cipriano <jony.12333@gmail.com>
2022-05-27 01:22:57 -0600

    CRUD DE PROVEEDORES Y SU CONSTRUCTOR

--- main/Empresa.h
+++ main/Empresa.h
@@ -1,5 +1,25 @@
 #pragma once
+#include <iostream>
 class Empresa
 {
+	// atributos
+protected:
+	int idProveedor = 0;
+	string proveedor, nit, direccion, telefono
+	//constructor
+protected:
+	Empresa() {}
+	Empresa(int idProve, string n, string dir, string tel) {
+		idProveedor = idProve;
+		//proveedor = prove;
+		nit = n;
+		direccion = dir;
+		telefono = tel;
+
+	}
+	void leer();
+	void crear();
+	void modificar();
+	void eliminar();
 };
 
--- main/Proveedor.h
+++ main/Proveedor.h
@@ -1,5 +1,144 @@
 #pragma once
-class Proveedor
-{
+#include <iostream>
+#include <mysql.h>
+#include "ConexionBD.h"
+#include <string>
+#include "Empresa.h"
+
+using namespace std;
+
+class Proveedor :
+	public Empresa {
+
+	// atributos
+private: string proveedor;
+	   // constructor
+public:
+	Proveedor() {}
+	Proveedor(int idProve, string prove, string n, string dir, string tel) : Empresa(idProve, n, dir, tel) {
+		proveedor = prove;
+
+	}
+	//set (modificar)
+	void setidProveedor(int idProve) { idProveedor = idProve; }
+	void setproveedor(string prove) { proveedor = prove; }
+	void setnit(string n) { nit = n; }
+	void setdireccion(string dir) { direccion = dir; }
+	void settelefono(string tel) { telefono = tel; }
+
+	//get (mostrar)
+	int getidProveedor() { return idProveedor; }
+	string getproveedor() { return proveedor; }
+	string getnit() { return nit; }
+	string getdireccion() { return direccion; }
+	int gettelefono() { return telefono; }
+
+	void crear() {
+		int q_estado;
+		Conector cn = Conector();
+		cn.conexion_abierta();
+		if (cn.getConectar()) {
+			string idProve = to_string(idProveedor);
+			string insert = "INSERT INTO `proyecto final super mercado`.Proveedores(proveedor,nit,direccion,telefono) VALUES('" + proveedor + "','" + nit + "','" + direccion + "'," + tel + "' )";
+			const char* i = insert.c_str();
+			q_estado = mysql_query(cn.getConectar(), i);
+			if (!q_estado) {
+				cout << "Datos ingresados correctamente " << endl;
+			}
+			else {
+				cout << "Error al ingresar " << endl;
+			}
+		}
+		else {
+			cout << "-----...ERROR EN LA CONEXION...-----" << endl;
+		}
+		cn.conexion_cerrada();
+
+	}
+
+
+
+	void update() {
+		int q_estado;
+
+		Conector cn = Conector();
+		cn.conexion_abierta();
+		if (cn.getConectar()) {
+			string idProve = to_string(idProveedor);
+			string actualizar = "UPDATE `proyecto final super mercado` SET proveedor = '" + proveedor + "', nit = '" + nit + "', direccion = '" + direccion + "', telefono = " + telefono + "' WHERE idProveedor = " + idProve + "";
+			const char* i = actualizar.c_str();
+			q_estado = mysql_query(cn.getConectar(), i);
+			if (!q_estado) {
+				cout << " Datos actualizados " << endl;
+			}
+			else {
+				cout << "Error al actualizar" << endl;
+			}
+
+		}
+		else {
+			cout << "-----...ERROR EN LA CONEXION...-----" << endl;
+		}
+		cn.conexion_cerrada();
+
+	}
+
+
+
+	void borrar() {
+		int q_estado;
+
+		Conector cn = Conector();
+		cn.conexion_abierta();
+		if (cn.getConectar()) {
+			string idProve = to_string(idProveedor);
+			string delet = "DELETE FROM `proyecto final super mercado`.Proveedores WHERE idProveedor = " + idProve + "";
+			const char* i = delet.c_str();
+			q_estado = mysql_query(cn.getConectar(), i);
+			if (!q_estado) {
+				cout << " Dato borrado " << endl;
+			}
+			else {
+				cout << "Error al borrar" << endl;
+			}
+
+		}
+		else {
+			cout << "-----...ERROR EN LA CONEXION...-----" << endl;
+		}
+		cn.conexion_cerrada();
+
+	}
+
+
+	void mostrar() {
+		int q_estado;
+		Conector cn = Conector();
+		MYSQL_ROW fila;
+		MYSQL_RES* resultado;
+		cn.conexion_abierta();
+		if (cn.getConectar()) {
+			string leer = "select * `proyecto final super mercado`.Proveedores ";
+			const char* c = leer.c_str();
+			q_estado = mysql_query(cn.getConectar(), c);
+			if (!q_estado) {
+				resultado = mysql_store_result(cn.getConectar());
+
+				while (fila = mysql_fetch_row(resultado)) {
+					cout << fila[0] << "," << fila[1] << "," << fila[2] << "," << fila[3] << fila[4] << endl;
+				}
+			}
+			else {
+				cout << " Error al mostrar los datos " << endl;
+			}
+		}
+		else {
+			cout << "-----...ERROR EN LA CONEXION...-----" << endl;
+		}
+		cn.conexion_cerrada();
+	}
 };
+
+
+
 
