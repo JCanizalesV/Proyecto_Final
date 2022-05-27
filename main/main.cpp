0b36230d95c312520aa2de26c0d531d8bb0747a4
JCanizalesV <venturadaniel71@gmail.com>
2022-05-27 00:31:31 -0600

    actualizacion main

--- main/main.cpp
+++ main/main.cpp
@@ -1,14 +1,27 @@
 #include <mysql.h>
 #include <iostream>
 #include "Puesto.h"
+#include "Empleado.h"
 #include "Marca.h"
+
 
 using namespace std;
 
 int main()
 {
-    string puesto, marca;
-    int id_puesto, id_marca;
+    //PUESTO
+    string puesto; 
+    int id_puesto; 
+
+    // MARCA
+    string marca; 
+    int id_marca;
+
+    //EMPLEADOS
+    string e_nombres, e_apellidos, e_direccion, e_dpi, e_fechanacimiento, e_fechainicio, e_fechaingreso;
+    int id_empleado, e_telefono, e_genero; 
+
+
     int menu;
 
     do
@@ -138,6 +151,107 @@ int main()
         if (menu == 2)
         {
             system("cls");
+
+            do
+            {
+                system("cls");
+
+                cout << "\n\n\t\t\tMENU EMPLEADOS" << endl;
+                cout << "\t\t\t------------" << endl << endl;
+                cout << "\n\t1. Ingresar un nuevo empleado al supermercado" << endl;
+                cout << "\t2. Mostrar puestos ingresados" << endl;
+                cout << "\t3. Modificar o actualizar un puesto ingresado" << endl;
+                cout << "\t4. Eliminar un puesto ingresado" << endl;
+                cout << "" << endl;
+                cout << "\tPRESIONE 11 SALIR AL MENU PRINCIPAL" << endl;
+
+                cout << "\n\tIngrese la opcion que desea realizar:       ";
+                cin >> menu;
+                cout << "" << endl;
+
+                if (menu == 1)
+                {
+                    system("cls");
+
+                    cout << "\n\n\t\t\tINGRESAR NUEVO EMPLEADO" << endl;
+                    cout << "\t\t\t---------------------" << endl << endl;
+                    cout << "\n\tIngrese nombres:          "; cin >> e_nombres;
+                    cout << "\n\tIngrese apellidos:          "; cin >> e_apellidos;
+                    cout << "\n\tIngrese direccion:          "; cin >> e_direccion;
+                    cout << "\n\tIngrese telefono:          "; cin >> e_telefono;
+                    cout << "\n\tIngrese DPI:          "; cin >> e_dpi;
+                    cout << "\n\tIngrese genero:          "; cin >> e_genero;
+                    cout << "\n\tIngrese fecha naci:          "; cin >> e_fechanacimiento;
+                    cout << "\n\tIngrese id_puesto:          "; cin >> id_puesto;
+                    cout << "\n\tIngrese fecha inicio:          "; cin >> e_fechainicio;
+                    cout << "\n\tIngrese fecha ingreso:          "; cin >> e_fechaingreso;
+
+                    cout << "" << endl;
+
+
+                    system("pause");
+                }
+
+                if (menu == 2)
+                {
+                    system("cls");
+
+
+                    cout << "\n\n\t\t\tPUESTOS INGRESADOS" << endl;
+                    cout << "\t\t\t------------------" << endl << endl;
+
+                    Puesto c = Puesto();
+                    c.leer();
+
+                    cout << "" << endl;
+                    system("pause");
+                }
+
+                if (menu == 3)
+                {
+                    system("cls");
+
+                    cout << "\n\n\t\t\tPUESTOS INGRESADOS" << endl;
+                    cout << "\t\t\t------------------" << endl << endl;
+
+                    Puesto c = Puesto(id_puesto, puesto);
+                    c.leer();
+
+                    cout << "\n\n\t\t\tACTUALIZAR O MODIFICAR PUESTO" << endl;
+                    cout << "\t\t\t-----------------------------" << endl << endl;
+
+                    cout << "\n\tIngrese ID que desea modificar:          "; cin >> id_puesto;
+                    cout << "\n\tActualice puesto:          "; cin >> puesto;
+                    cout << "" << endl;
+
+
+                    c.setID_puesto(id_puesto);
+                    c.setPuesto(puesto);
+
+                    c.modificar();
+
+                    cout << "" << endl;
+                    system("pause");
+                }
+
+                if (menu == 4)
+                {
+                    system("cls");
+
+
+                    cout << "\n\n\t\t\tELIMINAR PUESTO INGRESADO" << endl;
+                    cout << "\t\t\t-------------------------" << endl << endl;
+
+                    cout << "\n\tIngrese ID que desea eliminar:          "; cin >> id_puesto;
+                    cout << "" << endl;
+
+                    Puesto c = Puesto(id_puesto, puesto);
+                    c.eliminar();
+
+                    cout << "" << endl;
+                    system("pause");
+                }
+            } while (menu != 11);
 
             cout << "" << endl;
             system("pause");
