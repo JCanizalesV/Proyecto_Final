732d7af70f94abc92e87b82da64f2d70ea74214f
Jonatan-Cipriano <jony.12333@gmail.com>
2022-05-27 20:29:18 -0600

    Creacion de constructor y crud de la clase clientes

--- main/Persona_C.h
+++ main/Persona_C.h
@@ -1,5 +1,31 @@
 #pragma once
-class Persona_C
-{
-};
+#include <iostream>
+#include <mysql.h>
+#include "ConexionBD.h"
+#include <string>
 
+using namespace std;
+
+class Persona_C {
+	//atributos 
+protected:
+	string nombres, apellidos nit, telefono, correo_electronico, fechaingreso
+		int idCLiente = 0, genero = 0;
+	//Constructor
+protected:
+	Persona_C(int idc, string nom, string ape, string n, int gen, string tel, string correo, string fe_in) {
+		idCliente = idc;
+		nombres = nom;
+		apellidos = ape;
+		nit = n;
+		genero = gen;
+		telefono = tel;
+		correo_electronico = correo;
+		fechaingreso = fe_in;
+
+	}
+	void leer();
+	void crear();
+	void modificar();
+	void eliminar();
+};
